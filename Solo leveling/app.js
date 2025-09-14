1c8cbdb5606b65c7ca46bdb33803fe26ed70b730
Aryan <aryan182007@gmail.com>
2025-09-14 20:09:22 +0530

    Solo Leveling

--- Solo leveling/app.js
+++ Solo leveling/app.js
@@ -2,3 +2,43 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function firstpageanim(){
+    var tl = gsap.timeline();
+
+    tl.from("#nav", {
+        y: '-10',
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    })
+    .to(".boundingelem", {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration:2,
+        delay:-1,
+        stagger: .2
+    })
+    .from("#herofooter", {
+        y: -10,
+        opacity: 0,
+        duration: 1.5,
+        delay:-1,
+        ease: Expo.easeInOut
+    })
+}
+
+function circleChapta(){
+    window.addEventListener("mousemove",function(dets){
+
+    })
+}
+
+function circleMouseFollower(){
+    window.addEventListener("mousemove",function(dets){
+    //    console.log(dets.clientX,dets.clientY);
+    this.document.querySelector("#circle").style.transform = `translate(${dets.clientX}px,${dets.clientY}px)`});
+}
+
+circleMouseFollower();
+firstpageanim();
